a72676de173d74240361d655789e4a87852e5483
MatNimv <matilda.nimvik@gmail.com>
2023-08-20 12:29:56 +0200

    navbar scrolleffekt

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,33 @@
 import { Link } from 'react-router-dom';
 import '../styles/animations.css';
+import { useEffect, useState } from 'react';
 
 const NavBar = () => {
+    const [isShrunk, setShrunk] = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => {
+            setShrunk((isShrunk) => {
+                if(!isShrunk &&
+                    (document.body.scrollTop > 50 ||
+                        document.documentElement.scrollTop > 50)
+                        ){return true;}
+
+                        if(isShrunk && 
+                            document.body.scrollTop < 4 &&
+                            document.documentElement.scrollTop < 4)
+                            {return false}
+
+                            console.log(isShrunk);
+                            return isShrunk;
+            })
+        }
+        window.addEventListener("scroll", onScroll);
+        return () => window.removeEventListener("scroll", onScroll);
+    }, []);
+
     return ( 
-        <nav>
+        <nav className={`scrollis${isShrunk.toString()}`}>
             <h1>VirkSchuVik</h1>
             <div className='stroke'>
                 <Link to="/">Hem</Link>
