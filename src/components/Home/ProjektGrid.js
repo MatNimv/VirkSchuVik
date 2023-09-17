d6f6e30dbcde57e491870c7a94882840876bedcc
MatNimv <matilda.nimvik@gmail.com>
2023-09-17 18:33:42 +0200

    inlogg för alla, obs skapa

---
+++ src/components/Home/ProjektGrid.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react';
+import firebase from '../../firebase';
+
+
+const ProjektGrid = () => {
+    const [projekt, setProjekt] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const ref = firebase.firestore().collection("projekt");
+
+    function randomiseArr(array){
+        let i = array.length;
+        while (i > 0) {
+          const ri = Math.floor(Math.random() * i);
+            i--;
+            [array[i], array[ri]] = [array[ri], array[i]];
+        }
+        //kortar ner arrayen till griden
+        array.length = 3;
+        return array;
+    }
+
+    function getProjekt() {
+        setLoading(true);
+        ref.onSnapshot((QuerySnapshot) => {
+            let items = [];
+            QuerySnapshot.forEach((doc) => {
+                items.push(doc.data());
+            });
+            setProjekt(randomiseArr(items));
+            setLoading(false);
+            });
+        }
+
+        useEffect(() => {
+            getProjekt()
+        }, [])
+
+        if(loading){
+            return <h1>Laddar in projekt...</h1>
+        }
+
+        return ( 
+            <div id="ProjektGridWrapper">
+                <div id="gridWrapper">
+                    {projekt.map((pro) => (
+                        <div className='oneGrid' key={pro.id}>
+                        	<div className="card" style={{backgroundImage: `url(${pro.hero})`}}>
+                                <a href={'/projekt/' + pro.id}>
+                                    <div className="gradient" />
+                                    <div className="text">
+                                        <h2>{pro.title}</h2>
+                                        <div className="info-container">
+                                            <h5>av {pro.author}</h5>
+                                            <h5>{pro.dateShow}</h5>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+}
+
+export default ProjektGrid;
---
+++ src/components/Skapa/OBS.js
@@ -0,0 +1,14 @@
+const OBS = () => {
+
+    return ( 
+    <div className="OBSListWrapper">
+        <ul>
+            <li><div className='listPic'></div>Klipp inte in text. Tyvärr, formatet buggar och databasen kan inte koda av texten. Get to writing.</li>
+            <li><div className='listPic'></div>Bild i 1:1/fyrkant format. Jag var lat och fyrkant är lättast att hantera.</li>
+            <li><div className='listPic'></div>Klicka i ditt namn i författarlistan oavsett om det redan är "valt", annars står det ingen författare.</li>
+        </ul>
+    </div>
+    );
+}
+
+export default OBS;
