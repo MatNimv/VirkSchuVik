335dbce105932b8b41b356950996a2be30d213ff
MatNimv <matilda.nimvik@gmail.com>
2023-09-24 17:13:57 +0200

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,14 +5,20 @@ import { auth, db, logout } from "../firebase";
 import { query, collection, getDocs, where } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import getWindowDimensions from '../functions/getWindowDimensions';
+import useWindowDimensions from '../functions/getWindowDimensions';
 
 const NavBar = () => {
     const [isShrunk, setShrunk] = useState(false);
     const [user, loading, error] = useAuthState(auth);
     const [name, setName] = useState("");
     const currentUser = auth.currentUser;
+    const { height, width } = useWindowDimensions();
+
+    console.log(width);
 
     const [dropMenu, setDropMenu] = useState(false);
+    const [sideMenu, setSideMenu] = useState(false);
 
     async function fetchUserName() {
         if(currentUser){
@@ -31,6 +37,16 @@ const NavBar = () => {
 
     function handleDropMenu(){
         setDropMenu(!dropMenu);
+    }
+    function handleSideMenu(){
+        if (sideMenu === true){
+            setSideMenu(false);
+            console.log("false");
+        } else {
+            setSideMenu(true)
+            console.log("true");
+        }
+        //setSideMenu(!dropMenu);
     }
 
     //kolla om någon är inloggad eller ej
@@ -61,6 +77,42 @@ const NavBar = () => {
         return () => window.removeEventListener("scroll", onScroll);
     }, [user,loading]);
 
+    if(width < 732){
+        return ( 
+            <div>
+                <nav className='mobile'>
+                    <h1>VSV</h1>
+                    <div className='middlespace'></div>
+                    <div className='hamburger' onClick={handleSideMenu}>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+
+                    <div className={`sidebar openis${sideMenu}`}>
+                        { user && 
+                            <div className='userWrapper'>
+                                <p>Hej {name}!</p>
+                                <Link to="/Skapa">Skapa inlägg</Link>
+                                <p onClick={logout}>Logga ut</p> 
+                                <div className='divider'></div>
+                            </div>
+                        }
+                            <div className='links'>
+                                <Link to="/">Hem</Link>
+                                <Link to="/projekt">Projekt</Link>
+                                <Link to="/omOss">Om Oss</Link>
+                            </div>
+                    
+                    </div>
+                    <div className={`backdrop${sideMenu}`}></div>
+                </nav>
+            </div>
+        )
+    } else {
+
+    
+
     return ( 
         <nav className={`scrollis${isShrunk.toString()}`}>
             <h1>V S V</h1>
@@ -83,6 +135,7 @@ const NavBar = () => {
             </div>
         </nav>
     );
+}
 }
 
 export default NavBar;
